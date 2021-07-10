8c052c67f7a7eceb7810e599d4b9435f66941ab3
Naeem Hussain <naeemnh@outlook.com>
2021-07-10 12:20:05 +0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-const express = require('express'),
-	app = express();
+const app = require('express')(),
+	mongoose = require('mongoose'),
+	cookieSession = require('cookie-session'),
+	passport = require('passport'),
+	keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
 
-app.get('/', (req, res) => {
-	res.send({ hi: 'there' });
+mongoose.connect(keys.localMongoURI, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
 });
 
-const PORT = process.env.PORT || 5000;
+app.use(
+	cookieSession({
+		maxAge: 1000 * 60 * 60 * 24 * 30,
+		keys: [keys.cookieKey],
+	})
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT);
