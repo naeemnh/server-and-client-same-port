4a245d24152e39b5937260b659095e4ed9a2b052
Naeem Hussain <naeemnh@outlook.com>
2022-03-02 11:26:13 +0400

    minor bugs fixed and sendgrid key updated - vid-130 github-124

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,18 +1,20 @@
-const mongoose = require('mongoose'),
-	requireLogin = require('../middlewares/requireLogin'),
-	requireCredits = require('../middlewares/requireCredits'),
-	Survey = mongoose.model('surveys'),
-	surveyTemplate = require('../services/emailTemplates/surveyTemplate');
+const mongoose = require("mongoose"),
+	requireLogin = require("../middlewares/requireLogin"),
+	requireCredits = require("../middlewares/requireCredits"),
+	Survey = mongoose.model("surveys"),
+	surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 module.exports = (app) => {
-	app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+	app.post("/api/surveys", requireLogin, requireCredits, (req, res) => {
 		const { title, subject, body, recipients } = req.body;
 
 		const survey = new Survey({
 			title,
 			subject,
 			body,
-			recipients: recipients.split(',').map((email) => ({ email })),
+			recipients: recipients
+				.split(",")
+				.map((email) => ({ email: email.trim() })),
 			_user: req.user._id,
 			dateSent: Date.now(),
 		});
